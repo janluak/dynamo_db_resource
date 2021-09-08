46d6b4cbee5648de1a6765c9f5c9c1c10566f1e9
janluak <janluak@users.noreply.github.com>
2021-09-08 12:18:27 +0200

    fix tests to run correctly in GitHub action

--- tests/test_cast_infrastructure.py
+++ tests/test_cast_infrastructure.py
@@ -7,49 +7,54 @@ with open(Path(Path(__file__).parent, "test_data/tables/TableForInfrastructureTe
 
 def test_to_infrastructure_code():
     from dynamo_db_resource.table_existence import convert_schema_to_infrastructure_code
-    assert convert_schema_to_infrastructure_code(DATABASE_SCHEMA) == {
-        'AttributeDefinitions': [
-            {
-                'AttributeName': 'some_string',
-                'AttributeType': 'S'
-            },
-            {
-                'AttributeName': 'primary_partition_key',
-                'AttributeType': 'S'
-            }
-        ],
-        'KeySchema': [
-            {
-                'AttributeName': 'primary_partition_key',
-                'KeyType': 'HASH'
-            }
-        ],
-        'LocalSecondaryIndexes': [
-            {
-                'IndexName': 'some_string_index',
-                'KeySchema': [
-                    {
-                        'AttributeName': 'some_string',
-                        'KeyType': 'HASH'
-                    }
-                ],
-                'Projection': {
-                    'ProjectionType': 'ALL'
+    converted_schema = convert_schema_to_infrastructure_code(DATABASE_SCHEMA)
+
+    assert json.dumps(converted_schema, sort_keys=True) == json.dumps(
+        {
+            'AttributeDefinitions': [
+                {
+                    'AttributeName': 'some_string',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'primary_partition_key',
+                    'AttributeType': 'S'
                 }
-            }
-        ],
-        'ProvisionedThroughput': {
-            'ReadCapacityUnits': 1,
-            'WriteCapacityUnits': 1
+            ],
+            'KeySchema': [
+                {
+                    'AttributeName': 'primary_partition_key',
+                    'KeyType': 'HASH'
+                }
+            ],
+            'LocalSecondaryIndexes': [
+                {
+                    'IndexName': 'some_string_index',
+                    'KeySchema': [
+                        {
+                            'AttributeName': 'some_string',
+                            'KeyType': 'HASH'
+                        }
+                    ],
+                    'Projection': {
+                        'ProjectionType': 'ALL'
+                    }
+                }
+            ],
+            'ProvisionedThroughput': {
+                'ReadCapacityUnits': 1,
+                'WriteCapacityUnits': 1
+            },
+            'StreamSpecification': {
+                'StreamViewType': 'NEW_AND_OLD_IMAGES'
+            },
+            'TableName': 'TableForInfrastructureTest',
+            'Tags': [
+                {
+                    'Key': 'tag-Key',
+                    'Value': 'tag-Value'
+                }
+            ]
         },
-        'StreamSpecification': {
-            'StreamViewType': 'NEW_AND_OLD_IMAGES'
-        },
-        'TableName': 'TableForInfrastructureTest',
-        'Tags': [
-            {
-                'Key': 'tag-Key',
-                'Value': 'tag-Value'
-            }
-        ]
-    }
+        sort_keys=True
+    )
