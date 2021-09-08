c2db1e90224c3899527fddc8f2182cdb4ab43070
janluak <janluak@users.noreply.github.com>
2021-09-08 12:41:50 +0200

    cast table infrastructure: sort keys for consistent results on re-runs

--- tests/test_cast_infrastructure.py
+++ tests/test_cast_infrastructure.py
@@ -7,54 +7,49 @@ with open(Path(Path(__file__).parent, "test_data/tables/TableForInfrastructureTe
 
 def test_to_infrastructure_code():
     from dynamo_db_resource.table_existence import convert_schema_to_infrastructure_code
-    converted_schema = convert_schema_to_infrastructure_code(DATABASE_SCHEMA)
-
-    assert json.dumps(converted_schema, sort_keys=True) == json.dumps(
-        {
-            'AttributeDefinitions': [
-                {
-                    'AttributeName': 'some_string',
-                    'AttributeType': 'S'
-                },
-                {
-                    'AttributeName': 'primary_partition_key',
-                    'AttributeType': 'S'
-                }
-            ],
-            'KeySchema': [
-                {
-                    'AttributeName': 'primary_partition_key',
-                    'KeyType': 'HASH'
-                }
-            ],
-            'LocalSecondaryIndexes': [
-                {
-                    'IndexName': 'some_string_index',
-                    'KeySchema': [
-                        {
-                            'AttributeName': 'some_string',
-                            'KeyType': 'HASH'
-                        }
-                    ],
-                    'Projection': {
-                        'ProjectionType': 'ALL'
+    assert convert_schema_to_infrastructure_code(DATABASE_SCHEMA) == {
+        'AttributeDefinitions': [
+            {
+                'AttributeName': 'primary_partition_key',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'some_string',
+                'AttributeType': 'S'
+            }
+        ],
+        'KeySchema': [
+            {
+                'AttributeName': 'primary_partition_key',
+                'KeyType': 'HASH'
+            }
+        ],
+        'LocalSecondaryIndexes': [
+            {
+                'IndexName': 'some_string_index',
+                'KeySchema': [
+                    {
+                        'AttributeName': 'some_string',
+                        'KeyType': 'HASH'
                     }
+                ],
+                'Projection': {
+                    'ProjectionType': 'ALL'
                 }
-            ],
-            'ProvisionedThroughput': {
-                'ReadCapacityUnits': 1,
-                'WriteCapacityUnits': 1
-            },
-            'StreamSpecification': {
-                'StreamViewType': 'NEW_AND_OLD_IMAGES'
-            },
-            'TableName': 'TableForInfrastructureTest',
-            'Tags': [
-                {
-                    'Key': 'tag-Key',
-                    'Value': 'tag-Value'
-                }
-            ]
+            }
+        ],
+        'ProvisionedThroughput': {
+            'ReadCapacityUnits': 1,
+            'WriteCapacityUnits': 1
         },
-        sort_keys=True
-    )
+        'StreamSpecification': {
+            'StreamViewType': 'NEW_AND_OLD_IMAGES'
+        },
+        'TableName': 'TableForInfrastructureTest',
+        'Tags': [
+            {
+                'Key': 'tag-Key',
+                'Value': 'tag-Value'
+            }
+        ]
+    }
